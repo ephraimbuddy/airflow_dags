c8486af5c6571619168bf4c4a28185b7d4d45bd8
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-06-16 14:15:04 +0100

    make change

--- dags/bug3.py
+++ dags/bug3.py
@@ -12,7 +12,7 @@ dag = DAG(
 )
 
 hello_task = BashOperator(
-    task_id='test_task',
+    task_id='test_task1',
     bash_command='echo "Hello World from Airflow!"',
     do_xcom_push = True,
     dag=dag,
@@ -25,7 +25,7 @@ bye_task = BashOperator(
 )
 
 hello_again = BashOperator(
-    task_id='test_task_hello2',
+    task_id='test_task_hello',
     bash_command='echo "Hello World from Airflow!"',
     dag=dag,
 )
