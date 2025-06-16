d49c03e9ad69e9736b552157e503c9442021b33e
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-06-16 14:00:50 +0100

    add schedule

--- dags/bbug1.py
+++ dags/bbug1.py
@@ -1,7 +1,9 @@
 from airflow import DAG
+from datetime import datetime
 from airflow.providers.standard.operators.bash import BashOperator
 
-with DAG(dag_id="demo2"):
+with DAG(dag_id="demo2",start_date=datetime(2025, 5, 1, 3, 28, 0),
+    schedule='@daily'):
     # First run
     # sleep = BashOperator(task_id="sleep", bash_command="sleep 300")
     # hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
