0814940ffbaa576e9854855318d4852b4198f1bc
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-06-19 10:24:42 +0100

    update dag

--- dags/bbug1.py
+++ dags/bbug1.py
@@ -5,14 +5,14 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(dag_id="demo2",start_date=datetime(2025, 5, 1, 3, 28, 0),
     schedule='@daily'):
     # First run
-    sleep = BashOperator(task_id="sleep", bash_command="sleep 300")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    # sleep = BashOperator(task_id="sleep", bash_command="sleep 300")
+    # hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
+    # astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
     
-    sleep >> hello >> astronomer
+    # sleep >> hello >> astronomer
 
     # Second run
-    # sleep = BashOperator(task_id="sleep", bash_command="sleep 20")
-    # hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
+    sleep = BashOperator(task_id="sleep", bash_command="sleep 20")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
 
-    # sleep >> hello
+    sleep >> hello
