4ce8920dd60aa60197cacf9746e88cb2dd6d16a3
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-09-01 20:11:38 +0100

    update

--- dags/demo.py
+++ dags/demo.py
@@ -9,7 +9,7 @@ with DAG(dag_id="demo",start_date=datetime(2025, 5, 1),
     schedule='@daily', on_success_callback=_success):
     sleep = BashOperator(task_id="sleep", bash_command="echo 'Hello'")
     hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    #astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
     
     sleep >> hello >> astronomer
 
