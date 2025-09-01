59ed02bee3f915aac5207f6d256a0ec30c02d2e1
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-09-01 16:25:15 +0100

    update

--- dags/demo.py
+++ dags/demo.py
@@ -1,21 +1,22 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.providers.standard.operators.bash import BashOperator
+# datetime.now()-timedelta(days=1)
 
 def _success(context):
     print("Task succeeded!")
 
-with DAG(dag_id="demo",start_date=datetime.now()-timedelta(days=1),
+with DAG(dag_id="demo",start_date=datetime(2025, 5, 1),
     schedule='@daily', on_success_callback=_success):
     # First run
-    # sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    # hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    # astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
+    astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
     
-    # sleep >> hello >> astronomer
+    sleep >> hello >> astronomer
 
     # Second run
-    sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
+    # sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
+    # hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
 
-    sleep >> hello
+    # sleep >> hello
