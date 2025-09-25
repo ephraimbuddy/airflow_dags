7cb7e3b864212bae9d8a4a1d2f7b86d737ba8bfb
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-09-25 21:41:09 +0100

    fixup! fixup! fixup! alds

--- dags/demo.py
+++ dags/demo.py
@@ -6,9 +6,9 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(dag_id="demo",start_date=datetime(2025, 5, 1),
     schedule='@daily'):
     
-    sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    sleep = BashOperator(task_id="sleep_2", bash_command="sleep 1")
+    hello = BashOperator(task_id="hello_2", bash_command="echo 'Hello'")
+    astronomer = BashOperator(task_id="astronomer_2", bash_command="echo 'Astonomer'")
     
     sleep >> hello >> astronomer
 
