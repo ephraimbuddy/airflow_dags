3206cad1a89cbbcf9bc0d4de57227ade4abff562
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-09-25 20:52:26 +0100

    fixup! alds

--- dags/demo.py
+++ dags/demo.py
@@ -2,20 +2,13 @@ from airflow import DAG
 from datetime import datetime
 from airflow.providers.standard.operators.bash import BashOperator
 
-def _success(context):
-    print("Task succeeded!")
 
 with DAG(dag_id="demo",start_date=datetime(2025, 5, 1),
-    schedule='@daily', on_success_callback=_success):
-    # First run
-    # sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    # hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    # astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    schedule='@daily'):
     
-    # sleep >> hello >> astronomer
-
-    # Second run
     sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
+    #astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer'")
+    
+    sleep >> hello #>> astronomer
 
-    sleep >> hello
