9021ab09f53728ac4f48ce24e22a79e4404bfc3b
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-11-13 09:34:37 +0100

    update

--- dags/demo2.py
+++ dags/demo2.py
@@ -10,7 +10,7 @@ utils.traceback.print_exc()
 with DAG(dag_id="demo",start_date=datetime(2025, 5, 1),
     schedule='@daily'):
     
-    sleep = BashOperator(task_id="sleep_1", bash_command="sleep 600")
+    sleep = BashOperator(task_id="sleep_1", bash_command="sleep 700")
     hello = BashOperator(task_id="hello_1", bash_command="echo 'Hello'")
     astronomer = BashOperator(task_id="astronomer_1", bash_command="echo 'Astonomer'")
 
