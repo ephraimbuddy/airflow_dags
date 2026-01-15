acd67c7fe3356e5c94230ce0a3722748ca2fc384
Ephraim Anierobi <splendidzigy24@gmail.com>
2026-01-15 12:02:12 +0100

    dynamic date

--- dags/demo2.py
+++ dags/demo2.py
@@ -7,7 +7,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 
 with DAG(dag_id="demo",start_date=datetime.utcnow(),
-    schedule='@daily'):
+    schedule='* * * * *', catchup=False) as dag:
     total_var = Variable.get("total_var")
     for i in range(int(total_var)):
         task = BashOperator(task_id=f"task_{i}", bash_command=f"echo Task {i}")
